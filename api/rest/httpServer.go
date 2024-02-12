0fb6bf10314116d993f391baf363cba735de5029
Yaroslav Lysak <lysak.yaroslav00@yandex.ru>
2024-02-12 20:40:35 +0300

    testing origins

--- api/rest/httpServer.go
+++ api/rest/httpServer.go
@@ -76,7 +76,8 @@ func (s *HTTPServer) ConfigureCORS(allowedOrigins []string) {
 	slog.Info("cross-origin enabled for: " + strings.Join(allowedOrigins, ", "))
 
 	s.router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowAllOrigins: true,
+		//AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"OPTIONS", "GET", "PUT", "PATCH", "DELETE", "POST"},
 		AllowHeaders:     []string{"Origin", "Accept", "Content-Type", "Authorization", "X-Forwarded-*"},
 		ExposeHeaders:    []string{"Content-Length"},
